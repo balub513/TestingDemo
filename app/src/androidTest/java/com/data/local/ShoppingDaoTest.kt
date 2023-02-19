9480bd9fe8d108a8e5debd3c87d90252b6527789
manuram <sairammech345@gmail.com>
2023-02-19 18:20:28 +0530

    ------ >> ROOM DataBase Test Cases Handling - -Included google Truth Library and Junit

--- app/src/androidTest/java/com/data/local/ShoppingDaoTest.kt
+++ app/src/androidTest/java/com/data/local/ShoppingDaoTest.kt
@@ -10,6 +10,7 @@ import com.example.testingdemo.data.local.ShoppingItem
 import com.getOrAwaitValue
 import com.google.common.truth.Truth.assertThat
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.test.runBlockingTest
 import org.junit.After
 import org.junit.Before
@@ -50,7 +51,36 @@ class ShoppingDaoTest {
             val itemList = dao.getAllObservableItems().getOrAwaitValue()
             assertThat(itemList).contains(item)
         }
+    }
 
+    @Test
+    fun testDeleteShoppingItem() {
+        runBlocking {
+            val item1 = ShoppingItem("Banana", 10, 5, "imageUrl", 1)
+            val item2 = ShoppingItem("Apple", 10, 5, "imageUrl", 2)
+            val item3 = ShoppingItem("grape", 10, 5, "imageUrl", 3)
+            dao.insertItem(item1)
+            dao.insertItem(item2)
+            dao.insertItem(item3)
+            dao.deleteItem(item2)
+            val itemList = dao.getAllObservableItems().getOrAwaitValue()
+            assertThat(itemList).hasSize(2)
+        }
+    }
+
+    @Test
+    fun testTotalItemsPrice(){
+        runBlocking {
+            val item1 = ShoppingItem("Banana", 10, 5, "imageUrl", 1)
+            val item2 = ShoppingItem("Apple", 10, 10, "imageUrl", 2)
+            val item3 = ShoppingItem("grape", 10, 10, "imageUrl", 3)
+            dao.insertItem(item1)
+            dao.insertItem(item2)
+            dao.insertItem(item3)
+
+            val itemsTotalPrice = dao.getItemsTotalPrice().getOrAwaitValue {  }
+            assertThat(itemsTotalPrice).isEqualTo(250)
+        }
     }
 
 }
